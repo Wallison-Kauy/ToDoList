8c935f88e2a360ab63126fcea9faf5be723af2e4
WALLISON KAUY LUSTOSA ALVES <54756131+Wallison-Kauy@users.noreply.github.com>
2022-12-30 12:47:49 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,81 @@
+const { response } = require('express');
+const express = require('express'); 
+const { v4: uuid } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(request, response, next){
+  const { method, url } = request;
+  
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+  return next();
+}
+
+app.use(logRequests);
+
+app.get('/projects', (request,response) => {
+  const { title } = request.query;
+
+  const results = title
+   ? projects.filter(project => project.title.includes(title))
+   : projects;
+
+  return response.json(results);
+});
+
+app.post('/projects', (request,response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(),title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+
+app.put('/projects/:id', (request,response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id == id);
+
+  if (projectIndex < 0){
+    return response.status(400).json({ error: 'Não Encontrado'});
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  }
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id', (request,response) => {
+
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0){
+    return response.status(400).json({ error: 'Não Encontrado'});
+  }
+
+  projects.splice(projectIndex,1);
+
+  return response.status(204).send();
+});
+
+app.listen(3333, () => {
+  console.log('🚀  Back-End Starned!'); 
+});
